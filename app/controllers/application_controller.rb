a00bd3f11d6548d63897ea95c87ec841e08dfdc7
Sriram Rao <reach@sriramrao.com>
2026-06-03 22:59:16 -0700

    Fix admin mode visibility and menu order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   helper_method :admin_mode?
 
   def admin_mode?
-    Current.user&.admin? && session.fetch(:admin_mode, true)
+    authenticated? && Current.user&.admin? && session.fetch(:admin_mode, true)
   end
 
   # Changes to the importmap will invalidate the etag for HTML responses
